097e8cfa5e5711c4a1d815683f556a5253ea7d88
lipengzhou <lpzmail@163.com>
2020-12-08 21:24:22 +0800

    提取通用数据模型

--- realworld-api-express/model/user.js
+++ realworld-api-express/model/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const baseModel = require('./base-model')
 
 const userSchema = new mongoose.Schema({
+  ...baseModel,
   username: {
     type: String,
     required: true
@@ -20,14 +22,6 @@ const userSchema = new mongoose.Schema({
   image: {
     type: String,
     default: null
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
   }
 })
 
