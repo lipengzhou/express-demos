a7d3adb7f114bf9aaa16b09f083f901b55888a71
lipengzhou <lpzmail@163.com>
2020-12-08 22:32:26 +0800

    接口案例-用户注册-密码加密处理

--- realworld-api-express/model/user.js
+++ realworld-api-express/model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const baseModel = require('./base-model')
+const md5 = require('../util/md5')
 
 const userSchema = new mongoose.Schema({
   ...baseModel,
@@ -13,7 +14,9 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    set: value => md5(value),
+    select: false
   },
   bio: {
     type: String,
