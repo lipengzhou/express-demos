ebfb06f8c3f16d76d0aaac78ef73177c929d4b9d
lipengzhou <lpzmail@163.com>
2020-12-08 21:48:14 +0800

    接口案例-验证-基本使用

--- realworld-api-express/router/user.js
+++ realworld-api-express/router/user.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const userCtrl = require('../controller/user')
+const { body, validationResult } = require('express-validator')
+const { User } = require('../model')
 
 const router = express.Router()
 
@@ -7,7 +9,35 @@ const router = express.Router()
 router.post('/users/login', userCtrl.login)
 
 // 用户注册
-router.post('/users', userCtrl.register)
+router.post('/users', [ // 1. 配置验证规则
+  body('user.username')
+    .notEmpty().withMessage('用户名不能为空')
+    .custom(async username => {
+      const user = await User.findOne({ username })
+      if (user) {
+        return Promise.reject('用户名已存在')
+      }
+    }),
+  body('user.password').notEmpty().withMessage('密码不能为空'),
+  body('user.email')
+    .notEmpty().withMessage('邮箱不能为空')
+    .isEmail().withMessage('邮箱格式不正确')
+    .bail()
+    .custom(async email => {
+      const user = await User.findOne({ email })
+      if (user) {
+        return Promise.reject('邮箱已存在')
+      }
+    })
+], (req, res, next) => { // 2. 判断验证结果
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  next()
+}, userCtrl.register) // 3. 通过验证，执行具体的控制器处理
 
 // 获取当前登录用户
 router.get('/user', userCtrl.getCurrentUser)
