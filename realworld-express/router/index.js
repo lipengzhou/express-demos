7207852892e69fbfed34359c86a12e7a7cd38247
lipengzhou <lpzmail@163.com>
2020-12-15 18:40:59 +0800

    传统Web应用案例-提取Layout

--- realworld-express/router/index.js
+++ realworld-express/router/index.js
@@ -3,7 +3,17 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index.html')
+  res.render('index')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login', {
+    isLogin: true
+  })
+})
+
+router.get('/register', (req, res) => {
+  res.render('login')
 })
 
 module.exports = router
