b382a03c80a7ce45ea68db20ec4f99635e8ecb55
lipengzhou <lpzmail@163.com>
2020-12-15 19:09:34 +0800

    传统Web应用案例-导航链接处理

--- realworld-express/router/index.js
+++ realworld-express/router/index.js
@@ -16,4 +16,28 @@ router.get('/register', (req, res) => {
   res.render('login')
 })
 
+router.get('/settings', (req, res) => {
+  res.render('settings')
+})
+
+router.get('/editor', (req, res) => {
+  res.render('editor')
+})
+
+router.get('/editor/:articleId', (req, res) => {
+  res.render('editor')
+})
+
+router.get('/article/:articleId', (req, res) => {
+  res.render('article')
+})
+
+router.get('/profile/:username', (req, res) => {
+  res.render('profile')
+})
+
+router.get('/profile/:username/favorites', (req, res) => {
+  res.render('profile')
+})
+
 module.exports = router
