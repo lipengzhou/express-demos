7a34f69b364021c1e670f77233e12e1d5296717e
lipengzhou <lpzmail@163.com>
2020-12-21 15:37:44 +0800

    传统Web应用案例-展示文章列表

--- realworld-express/controller/article.js
+++ realworld-express/controller/article.js
@@ -1,14 +1,31 @@
 const { Article } = require("../model")
 
-exports.showIndex = (req, res, next) => {
+exports.showIndex = async (req, res, next) => {
   try {
-    res.render('index')
+    const page = req.query.page
+      ? Number.parseInt(req.query.page)
+      : 1
+    const pageSize = 10
+
+    const articles = await Article.find()
+      .skip((page - 1) * pageSize) // 跳过多少
+      .limit(pageSize) // 取多少
+
+    const articlesCount = await Article.count()
+
+    res.render('index', {
+      articles,
+      page,
+      pageSize,
+      articlesCount,
+      totalPage: Math.ceil(articlesCount / pageSize)
+    })
   } catch (err) {
     next(err)
   }
 }
 
-exports.showEditor = (req, res, next) => {
+exports.showEditor = async (req, res, next) => {
   try {
     res.render('editor')
   } catch (err) {
@@ -16,9 +33,12 @@ exports.showEditor = (req, res, next) => {
   }
 }
 
-exports.showArticle = (req, res, next) => {
+exports.showArticle = async (req, res, next) => {
   try {
-    res.render('article')
+    const article = await Article.findById(req.params.articleId).populate('author')
+    res.render('article', {
+      article
+    })
   } catch (err) {
     next(err)
   }
